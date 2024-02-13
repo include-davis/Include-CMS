4f75a178547b63990e5b1e2834f8bfa82de4cb29
Austin Shih <austinhshih@gmail.com>
2024-02-12 22:36:37 -0800

    Update page.tsx

--- app/(pages)/pokemon/page.tsx
+++ app/(pages)/pokemon/page.tsx
@@ -6,7 +6,7 @@ import PokemonList from './_components/PokemonList/PokemonList';
 export default function Pokemon() {
   return (
     <div className={styles.body}>
-      <h1>Test out some of our APIs</h1>
+      <h1>Let's manage some pokemon!</h1>
       <CreatePokemonForm />
       <PokemonList />
     </div>
