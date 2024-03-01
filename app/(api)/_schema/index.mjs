79d25dd3176b86c8476073e20ed98bf0d42f2fef
Divya Vaddavalli <divyavaddavalli@campus-004-217.ucdavis.edu>
2024-03-01 10:43:24 -0800

    index addition

--- app/(api)/_schema/index.mjs
+++ app/(api)/_schema/index.mjs
@@ -1,9 +1,11 @@
-import Pokemon from './Pokemon.mjs';
-import Trainer from './Trainer.mjs';
+import Media from "./media.mjs";
+import Pokemon from "./Pokemon.mjs";
+import Trainer from "./Trainer.mjs";
 
 const schema = {
   pokemon: Pokemon,
   trainers: Trainer,
+  media: Media,
 };
 
 export default schema;
