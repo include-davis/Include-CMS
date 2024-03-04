44eee53ce860e004d594a8591af38b64ab906a90
Divya Vaddavalli <divyavaddavalli@Divyas-MacBook-Pro.local>
2024-03-03 22:39:56 -0800

    linter errors fixed

--- app/(api)/_schema/index.mjs
+++ app/(api)/_schema/index.mjs
@@ -1,6 +1,6 @@
-import Media from "./media.mjs";
-import Pokemon from "./Pokemon.mjs";
-import Trainer from "./Trainer.mjs";
+import Media from '/media.mjs';
+import Pokemon from './Pokemon.mjs';
+import Trainer from './Trainer.mjs';
 
 const schema = {
   pokemon: Pokemon,
