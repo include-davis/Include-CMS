3a868c446f8fdcb418498c93556f16e554387ea5
Divya Vaddavalli <divyavaddavalli@campus-052-056.ucdavis.edu>
2024-04-09 19:24:35 -0700

    require token

--- app/(api)/_datalib/auth/createToken.mjs
+++ app/(api)/_datalib/auth/createToken.mjs
@@ -1,4 +1,6 @@
-const token = jwt.sign({email}, process.env.JWT_SECRET, { expiresIn: "24hrs" })
+const jwt = require('jsonwebtoken');
+const token = jwt.sign({email}, process.env.JWT_SECRET, { expiresIn: "1800s" })
+
         return res
           .status(200)
           .json({ message: "User Logged in Successfully", token });
