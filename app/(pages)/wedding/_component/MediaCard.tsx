ef0837eac52de039dd223caec92062ba2a489a2d
Kelly Phan <kelly@campus-057-075.ucdavis.edu>
2024-04-09 18:56:29 -0700

    changing up media card'

---
+++ app/(pages)/wedding/_component/MediaCard.tsx
@@ -0,0 +1,23 @@
+'use client';
+import styles from '../../uploaded-media/_components/MediaCard.module.scss';
+import Image from 'next/image';
+
+export default function MediaCardPlaceholder() {
+    return (
+    <div className={styles.media_container}>
+        <div className={styles.title}>
+            <p> Wedding.png  </p>
+        </div>
+        <div className={styles.img_container}>
+            <Image 
+            src="/index/city.jpg"
+            alt="serene forest scene in the night"
+            layout="fill" 
+            objectFit="cover" 
+            className={styles.media_radius} 
+            />        
+        </div>
+    </div>
+    );
+  }
+  
--- app/(pages)/wedding/page.jsx
+++ app/(pages)/wedding/page.jsx
@@ -1,7 +1,10 @@
+import MediaCardPlaceholder from "./_component/MediaCard";
 export default function Home() {
   return (
     <main>
       <h1>Wedding</h1>
+      <MediaCardPlaceholder />
+
     </main>
   );
 }
