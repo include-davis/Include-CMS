ef62eda57a1f49caca2068095a5f4e13c42801b6
Divya Vaddavalli <divyavaddavalli@Divyas-MacBook-Pro.local>
2024-04-10 18:38:44 -0700

    remove secret

--- app/(api)/_datalib/auth/createToken.mjs
+++ app/(api)/_datalib/auth/createToken.mjs
@@ -1,4 +1,4 @@
-import jwt, {Secret} from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 export async function createAuthToken(data) {
     return jwt.sign(data, process.env.JWT_SECRET,{
       expiresIn: "24h",
