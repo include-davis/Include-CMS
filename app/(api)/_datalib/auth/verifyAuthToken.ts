5a2818b625ccdc99709def17ed9bdc6fda10103c
zbrandon98 <bdzarate@ucdavis.edu>
2024-04-14 21:40:54 -0700

    added verifyAuthToken.ts, but needs testing

---
+++ app/(api)/_datalib/auth/verifyAuthToken.ts
@@ -0,0 +1,17 @@
+import jwt, { Secret } from 'jsonwebtoken';
+import HttpError from '@utils/response/HttpError';
+
+export async function verifyAuthToken(token: string) {
+  try {
+    const verifiedPayload = jwt.verify(token, process.env.JWT_SECRET as Secret);
+
+    if (!verifiedPayload) {
+      throw new HttpError('Unauthorized. Invalid token.');
+    }
+
+    return {ok: true, body: verifiedPayload};
+  } catch (e) {
+    return {ok: false, error: e as HttpError};
+    }
+};
+
