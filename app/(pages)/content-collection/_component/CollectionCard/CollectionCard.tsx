d32cb223a98af0b79513bb2a4909beba63d36e67
foramms <foramahi2003@gmail.com>
2024-04-20 20:09:50 -0700

    updating Collections: MediaPreview

--- app/(pages)/content-collection/_component/CollectionCard/CollectionCard.tsx
+++ app/(pages)/content-collection/_component/CollectionCard/CollectionCard.tsx
@@ -1,8 +1,14 @@
 'use client';
 import styles from './CollectionCard.module.scss';
 import Image from 'next/image';
+import MediaPreview from './MediaPreview';
 
-export default function CollectionCard() {
+interface CollectionCardProps {
+    images: string[];
+  }
+  
+
+export default function CollectionCard({ images }: CollectionCardProps) {
    return (
    <div className={styles.media_container}>
         <div className={styles.edit_container}>
@@ -10,25 +16,10 @@ export default function CollectionCard() {
                 <div className={styles.title}>
                     <p> Wedding name 1 </p>
                 </div>
-                <Image 
-                src="/index/select-icon.png"
-                alt="serene forest scene in the night"
-                height={20}
-                width={4}
-                className={styles.select} 
-                />
             </div>
             <p>last edited: 11-03-24</p>
         </div>
-        <div className={styles.img_container}>
-            <Image 
-            src="/index/city.jpg"
-            alt="serene forest scene in the night"
-            layout="fill" 
-            objectFit="cover" 
-            className={styles.media_radius} 
-            />        
-        </div>
+        <MediaPreview images={images} />  
         <div className={styles.description}>
             <p> A short description that the user inputs in the next screen. </p>
         </div>
--- app/(pages)/content-collection/page.jsx
+++ app/(pages)/content-collection/page.jsx
@@ -1,13 +1,42 @@
+// import CollectionCard from "./_component/CollectionCard/CollectionCard";
+// import MediaCardPlaceholder from "./_component/CollectionCard/MediaCard";
+// import PreviwCardPlaceholder from "./_component/CollectionCard/PreviewCard";
+// export default function Home() {
+//   return (
+//     <main>
+//       <h1>Wedding</h1>
+//       <CollectionCard />
+//       <MediaCardPlaceholder />
+//       <PreviwCardPlaceholder />
+//     </main>
+//   );
+// }
+import React from "react";
 import CollectionCard from "./_component/CollectionCard/CollectionCard";
-import MediaCardPlaceholder from "./_component/CollectionCard/MediaCard";
-import PreviwCardPlaceholder from "./_component/CollectionCard/PreviewCard";
+
 export default function Home() {
+  const images = [
+    "/index/city.jpg",
+    "/index/city.jpg",
+    "/index/city.jpg",
+    "/index/city.jpg",
+  ];
+  const images2 = [
+    "/index/city.jpg",
+    "/index/city.jpg",
+  ];
+  const images3 = [
+    "/index/city.jpg",
+  ];
+
   return (
     <main>
       <h1>Wedding</h1>
-      <CollectionCard />
-      <MediaCardPlaceholder />
-      <PreviwCardPlaceholder />
+      <CollectionCard images={images} />
+      <CollectionCard images={images2} />
+      <CollectionCard images={images3} />
+      {/* <MediaCardPlaceholder />
+      <PreviewCardPlaceholder /> */}
     </main>
   );
 }
