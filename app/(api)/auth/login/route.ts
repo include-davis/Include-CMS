2457a4db02a9d2772fbe914e7ec141281f0b7047
zbrandon98 <bdzarate@ucdavis.edu>
2024-04-23 18:14:05 -0700

    added POST for login in (api)/auth/login. Added @typeDefs to paths in config. Added temporary Login.ts function

---
+++ app/(api)/auth/login/route.ts
@@ -0,0 +1,35 @@
+'use server';
+import { NextRequest, NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
+import jwt from 'jsonwebtoken';
+import { Login } from '@datalib/auth/login';
+import { HttpError, NotAuthenticatedError } from '@utils/response/Errors';
+import type AuthTokenInt from '@typeDefs/authToken';
+
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json();
+    const res = await Login(body);
+    const data = await res.json();
+
+    if (!data.ok) {
+      throw new NotAuthenticatedError(data.error);
+    }
+
+    const payload = jwt.decode(data.body) as AuthTokenInt;
+    cookies().set({
+      name: 'auth_token',
+      value: data.body,
+      expires: payload.exp * 1000,
+      secure: true,
+      httpOnly: true,
+    });
+    return NextResponse.json({ ok: true, body: payLoad }, { status: 200 });
+  } catch (e) {
+    const error = e as HttpError;
+    return NextResponse.json(
+      { ok: false, error: error.message },
+      { status: error.status || 400 }
+    );
+  }
+}
