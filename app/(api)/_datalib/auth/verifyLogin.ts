004e4c93aa706f2ec46fd0a35d05245995ebb41b
cesium <clopezandrade@ucdavis.edu>
2024-05-07 15:33:05 -0700

    Modified verifyLogin formatting to pass linter

--- app/(api)/_datalib/auth/verifyLogin.ts
+++ app/(api)/_datalib/auth/verifyLogin.ts
@@ -7,34 +7,34 @@ import jwt, { Secret } from 'jsonwebtoken';
  * @param storedHash The hash stored in the database.
  * @returns True if the password matches the hash, false otherwise.
  */
-export async function comparePasswordHash(providedPassword: string, storedHash: string): Promise<boolean> {
-    return bcrypt.compare(providedPassword, storedHash);
+export async function comparePasswordHash(
+  providedPassword: string,
+  storedHash: string
+): Promise<boolean> {
+  return bcrypt.compare(providedPassword, storedHash);
 }
 
-export async function verifyLogin(body: {email: string; password: string}){
-    try {
-
-        // If unsuccessful login of user or password, return AuthenticationError.
-        // getUserByEmail TO BE IMPLEMENTED.
-        const user = await getUserByEmail(email);
-        if(!user){
-            return {ok: false, error: 'AuthenticationError'};
-        }
-        const isMatch = await comparePasswordHash(user.password, user.passwordHash);
-        if (!isMatch) {
-            return { ok: false, error: 'AuthenticationError' };
-        }
-
-        // If successful login, generate token.
-        const secretKey: Secret = process.env.JWT_SECRET_KEY || 'secret_key';
-        const token = jwt.sign(
-            {userId: user.id, email: user.email},
-            secretKey,
-            {expiresIn: '24h'}
-        );
-        return {ok: true, data: token};
-    } catch (error) {
-        console.error('Error during login:', error);
-        return {ok: false, error: 'Internal server error'};
+export async function verifyLogin(email: string, password: string) {
+  try {
+    // If unsuccessful login of user or password, return AuthenticationError.
+    // getUserByEmail TO BE IMPLEMENTED.
+    const user = await getUserByEmail(email);
+    if (!user) {
+      return { ok: false, error: 'AuthenticationError' };
     }
+    const isMatch = await comparePasswordHash(password, user.passwordHash);
+    if (!isMatch) {
+      return { ok: false, error: 'AuthenticationError' };
+    }
+
+    // If successful login, generate token.
+    const secretKey: Secret = process.env.JWT_SECRET_KEY || 'secret_key';
+    const token = jwt.sign({ userId: user.id, email: user.email }, secretKey, {
+      expiresIn: '24h',
+    });
+    return { ok: true, data: token };
+  } catch (error) {
+    console.error('Error during login:', error);
+    return { ok: false, error: 'Internal server error' };
+  }
 }
