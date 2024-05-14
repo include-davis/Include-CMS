82e1459aa21ea1a61ff0e72b9583860b76c663fd
zbrandon98 <bdzarate@ucdavis.edu>
2024-05-13 20:09:58 -0700

    fixed function to match provided function from Judging App

--- app/(api)/_utils/auth/authFromRequest.ts
+++ app/(api)/_utils/auth/authFromRequest.ts
@@ -1,28 +1,22 @@
 import { NextRequest } from 'next/server';
-import { verifyAuthToken } from '@datalib/auth/verifyAuthToken';
-import HttpError from '@utils/response/HttpError';
-import jwt from 'jsonwebtoken';
+import jwt, { Secret } from 'jsonwebtoken';
 import NotAuthenticatedError from '@utils/response/NotAuthenticatedError';
-import type AuthTokenInt from '@typeDefs/authToken';
+import type DecodedToken from '@datatypes/DecodedToken';
 
 export async function authFromRequest(request: NextRequest) {
-  try {
-    const body = await request.json();
-    const token = body.cookies().get('auth_cookie');
-    const response = await verifyAuthToken(token);
-    if (!response.ok) {
-      throw new NotAuthenticatedError('Unauthorized. Invalid token.');
-    }
-
-    if (typeof response.body !== 'string') {
-      throw new NotAuthenticatedError('Token is invalid or not present');
-    }
-    const decoded_token = jwt.decode(response.body) as AuthTokenInt;
-    if (!decoded_token) {
-      throw new NotAuthenticatedError('Failed to decode token.');
-    }
-    return { ok: true, data: decoded_token, error: null };
-  } catch (e) {
-    return { ok: false, body: null, error: e as HttpError };
+  const token = request.cookies.get('auth_token')?.value;
+  if (!token) {
+    throw new NotAuthenticatedError('User not authenticated');
   }
+
+  const decoded = jwt.verify(
+    token,
+    process.env.JWT_SECRET as Secret
+  ) as DecodedToken;
+
+  if (decoded.exp * 1000 < Date.now()) {
+    throw new NotAuthenticatedError('Authentication expired');
+  }
+
+  return decoded;
 }
