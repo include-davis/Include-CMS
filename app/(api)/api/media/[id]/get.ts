abdb6ed49f61abea572921160ffb05dc635d0a7c
atfdavis <alexfong615@gmail.com>
2024-05-14 18:02:27 -0700

    actualy fixed post.ts error

--- app/(api)/api/media/[id]/get.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest } from 'next/server';
-import { findMediaItem } from '../../../_datalib/media/findMediaItem';
-
-export function GET(_: NextRequest, { params }: { params: { id: string } }) {
-  return findMediaItem(params.id);
-}
--- app/(api)/api/testCollection/post.ts
+++ app/(api)/api/testCollection/post.ts
@@ -1,7 +1,7 @@
-// import { NextRequest } from 'next/server';
-// import createCollectionItem from '../../_datalib/collections/createCollectionItem';
+import { NextRequest } from 'next/server';
+import createCollectionItem from '../../_datalib/collections/createCollectionItem';
 
-// export async function POST(request: NextRequest) {
-//   const body = await request.json();
-//   return createCollectionItem('testCollection', body);
-// }
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  return createCollectionItem('testCollection', body);
+}
