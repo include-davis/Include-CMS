c3852c063ddcdd3fb180d9a4c721a22dfcae062b
atfdavis <alexfong615@gmail.com>
2024-05-20 21:45:53 -0700

    fixed [collection_type]/post.ts

---
+++ app/(api)/api/media/[id]/get.ts
@@ -0,0 +1,7 @@
+import { findMediaItem } from '../../../_datalib/media/findMediaItem';
+import { useRouter } from 'next/router';
+
+export async function GET() {
+  const router = useRouter();
+  return findMediaItem(router.query.id);
+}
---
+++ app/(api)/api/media/[id]/post.ts
@@ -0,0 +1,7 @@
+import { NextRequest } from 'next/server';
+import createMediaItem from '../../../_datalib/media/createMediaItem';
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  return createMediaItem(body);
+}
