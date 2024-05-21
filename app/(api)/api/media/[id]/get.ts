71b07d79618f3e422de089f0813edacbddf14d77
atfdavis <alexfong615@gmail.com>
2024-05-20 21:51:51 -0700

    actually actually fixed [collection_type]/post.ts

--- app/(api)/api/media/[id]/get.ts
+++ app/(api)/api/media/[id]/get.ts
@@ -1,8 +1,8 @@
 // import { NextRequest } from 'next/server';
-import { findMediaItem } from '../../../_datalib/media/findMediaItem';
-import { useRouter } from 'next/router';
+// import { findMediaItem } from '../../../_datalib/media/findMediaItem';
+// import { useRouter } from 'next/router';
 
-export async function GET() {
-  const router = useRouter();
-  return findMediaItem(router.query.id);
-}
+// export async function GET() {
+//   const router = useRouter();
+//   return findMediaItem(router.query.id);
+// }
--- app/(api)/api/media/[id]/route.ts
+++ app/(api)/api/media/[id]/route.ts
@@ -1,2 +1,2 @@
-export { GET } from './get';
-export { POST } from './post';
+// export { GET } from './get';
+// export { POST } from './post';
