26b9b739ede7f43135b3671caa139f24a770f2a2
Win Cheng <winzamark12@gmail.com>
2024-06-18 22:10:26 -0700

    init updateCollectionItem

---
+++ app/(api)/_datalib/collections/updateCollectionItem.ts
@@ -0,0 +1,30 @@
+import { getDatabase } from '@utils/mongodb/mongoClient.mjs';
+import { NotFoundError } from '@utils/response/Errors';
+
+export async function updateCollectionItem(collection: string, query = {}, update = {}) {
+    try{
+        const db = await getDatabase();
+        const reqCollection = await db.collection(collection);
+        const reqDocument = await reqCollection.findOneAndUpdate(query, update);
+
+        if (!reqDocument || reqDocument.length === 0) {
+        throw new NotFoundError(`No Items Found.`);
+        }
+
+        return { ok: true, body: reqDocument, error: null}
+    } catch (error) {
+        if (error instanceof NotFoundError) {
+            return {
+                ok: false,
+                body: null,
+                error: { code: error.status, message: error.message },
+            };
+        } else {
+            return {
+                ok: false,
+                body: null,
+                error: { code: 500, message: 'Internal Server Error' },
+            };
+        }
+    }
+}
