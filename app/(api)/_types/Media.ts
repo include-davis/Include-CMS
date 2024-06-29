d725f6350e8900cb9c14c60c3bbbdb0f9f7c5081
brandonw504 <brandonw504@outlook.com>
2024-06-29 15:22:40 -0700

    Data seeding

---
+++ app/(api)/_types/Media.ts
@@ -0,0 +1,11 @@
+interface Media {
+  _id: string;
+  media_type: string;
+  name: string;
+  alt_text: string;
+  media_url: string;
+  preview_url: string;
+  date_added: Date;
+}
+
+export default Media;
