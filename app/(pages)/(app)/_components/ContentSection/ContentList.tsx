7cbafc3792257609dfc2a4abe3262d639e10125d
Austin Shih <austinhshih@gmail.com>
2024-07-06 17:51:01 -0700

    custom scroll progress finished

--- app/(pages)/(app)/_components/ContentSection/ContentList.tsx
+++ app/(pages)/(app)/_components/ContentSection/ContentList.tsx
@@ -1,20 +1,90 @@
+import { useRef, createRef, useEffect, useState } from 'react';
 import styles from './ContentList.module.scss';
+import { IoChevronBackOutline, IoChevronForwardOutline } from 'react-icons/io5';
 
 interface ContentListProps {
   expanded: boolean;
-  children: React.ReactNode;
+  children: React.ReactNode[];
 }
 
 export default function ContentList({ expanded, children }: ContentListProps) {
+  const viewportRef = useRef<HTMLDivElement>(null);
+  const contentBeltRef = useRef<HTMLDivElement>(null);
+  const cardRefs = children.map((_) => createRef<HTMLDivElement>());
+  const [progressWidth, setProgressWidth] = useState(0);
+  const [progressShift, setProgressShift] = useState(0);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (viewportRef.current && contentBeltRef.current) {
+        const { clientWidth: viewportWidth } = viewportRef.current;
+        const { clientWidth: contentWidth } = contentBeltRef.current;
+
+        setProgressWidth((viewportWidth / contentWidth) * 100);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [viewportRef, contentBeltRef]);
+
+  const handleScroll = () => {
+    if (viewportRef.current && contentBeltRef.current) {
+      const { clientWidth: viewportWidth, scrollLeft: viewportScroll } =
+        viewportRef.current;
+      const { clientWidth: contentWidth } = contentBeltRef.current;
+
+      const scrollableWidth = contentWidth - viewportWidth;
+
+      setProgressShift(
+        (viewportScroll / scrollableWidth) *
+          100 *
+          (1 - viewportWidth / contentWidth)
+      );
+    }
+  };
+
+  const progressStyles = {
+    width: `${progressWidth}%`,
+    left: `${progressShift}%`,
+  };
+
   return (
     <div className={styles.container}>
-      <div className={styles.viewport}>
-        <div className={`${styles.cards} ${expanded ? styles.expanded : null}`}>
-          {children}
+      <div
+        className={styles.viewport}
+        ref={viewportRef}
+        onScroll={handleScroll}
+      >
+        <div
+          className={`${styles.cards} ${expanded ? styles.expanded : null}`}
+          ref={contentBeltRef}
+        >
+          {children.map((child: any, index: number) => (
+            <div
+              className={styles.card_wrapper}
+              key={index}
+              ref={cardRefs[index as any]}
+            >
+              {child}
+            </div>
+          ))}
         </div>
       </div>
-      <div className={styles.controls}>
-        {'(<)'} CAROUSEL CONTROLS {'(>)'}
+      <div className={`${styles.controls} ${expanded ? styles.hidden : null}`}>
+        <button>
+          <IoChevronBackOutline className={styles.icon} />
+        </button>
+        <div className={styles.progress_container}>
+          <div className={styles.progress_indicator} style={progressStyles} />
+        </div>
+        <button>
+          <IoChevronForwardOutline className={styles.icon} />
+        </button>
       </div>
     </div>
   );
