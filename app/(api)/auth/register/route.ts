ba97820c67739c6af0708a780fb0fb0f66db2afd
Ryan <ryyu444@gmail.com>
2024-07-15 19:24:02 -0700

    added register route

---
+++ app/(api)/auth/register/route.ts
@@ -0,0 +1,39 @@
+'use server';
+import jwt from 'jsonwebtoken';
+import { NextRequest, NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
+import { Register } from '@datalib/auth/register';
+import { HttpError } from '@utils/response/Errors';
+import type { AuthTokenInt } from '@datatypes/authToken';
+import type { UserCredentials } from '@typeDefs/UserCredentials';
+
+export async function POST(request: NextRequest) {
+  try {
+    const body: UserCredentials = await request.json();
+    const res = await Register(body);
+    const data = await res.json();
+
+    if (!data.ok) {
+      throw new HttpError(data.error);
+    }
+
+    const payload = jwt.decode(data.body) as AuthTokenInt;
+    cookies().set({
+      name: 'auth_token',
+      value: data.body,
+      expires: payload.exp * 1000,
+      secure: true,
+      httpOnly: true,
+    });
+    return NextResponse.json(
+      { ok: true, body: payload, error: null },
+      { status: 200 }
+    );
+  } catch (e) {
+    const error = e as HttpError;
+    return NextResponse.json(
+      { ok: false, body: null, error: error.message },
+      { status: error.status || 400 }
+    );
+  }
+}
