d4020f7dffa0cdb6e6099d159788e7307ac88f4e
Austin Shih <austinhshih@gmail.com>
2024-08-25 22:13:45 -0700

    Update 20240826042413-create-content-collections.js

--- database/migrations/20240826042413-create-content-collections.js
+++ database/migrations/20240826042413-create-content-collections.js
@@ -1,18 +1,68 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const schema = require('../../schema/_index.js');
-console.log(JSON.stringify(schema));
+
+const processName = (name) => {
+  return name.toLowerCase().split(' ').join('_');
+};
+
+const typeMapping = {
+  shortText: {
+    bsonType: 'string',
+    description: 'Must be a string and is required',
+  },
+  longText: {
+    bsonType: 'string',
+    description: 'Must be a string and is required',
+  },
+  date: {
+    bsonType: 'string',
+    description: 'Must be a string and is required',
+  },
+  mediaItem: {
+    bsonType: 'objectId',
+  },
+  mediaList: {
+    bsonType: 'array',
+    items: {
+      bsonType: 'objectId',
+    },
+  },
+};
+
+const collections = Object.keys(schema);
+const collectionSchemas = collections.map((c) => schema[c]);
 
 module.exports = {
   async up(db, _) {
-    // TODO write your migration here.
-    // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
-    // Example:
-    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
+    collectionSchemas.forEach(async (collectionSchema) => {
+      const fields = collectionSchema.fields;
+      const fieldNames = fields.map((f) => processName(f.name));
+      const generatedProps = {};
+      fields.forEach((field) => {
+        generatedProps[processName(field.name)] = typeMapping[field.type.name];
+      });
+      const generatedSchema = {
+        $jsonSchema: {
+          bsonType: 'object',
+          required: fieldNames,
+          properties: {
+            _id: {
+              bsonType: 'objectId',
+            },
+            ...generatedProps,
+          },
+        },
+      };
+
+      const collectionName = processName(collectionSchema.name);
+      await db.createCollection(collectionName, { validator: generatedSchema });
+    });
   },
 
   async down(db, _) {
-    // TODO write the statements to rollback your migration (if possible)
-    // Example:
-    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
+    collectionSchemas.forEach(async (collectionSchema) => {
+      const collectionName = processName(collectionSchema.name);
+      await db.collection(collectionName).drop();
+    });
   },
 };
